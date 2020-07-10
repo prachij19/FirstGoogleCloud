4b1733808cbde3ff18cc5af122c702a241d90e0b
Prachi jain <prach@Prachi-laptop.prachi-lp>
2020-07-10 16:54:09 -0700

    fist commit

---
+++ src/main/java/com/cloud/july/FirstGoogleCloudApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.july;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGoogleCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGoogleCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/july/FirstGoogleCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.july;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGoogleCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
